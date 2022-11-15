90e2e4f59367f46596bc2a21049ca5106279df90
JeanRocha91 <jean.rocha.mt@gmail.com>
2022-11-15 18:26:27 +0000

    Update ubuinst.sh

--- install/ubuinst.sh
+++ install/ubuinst.sh
@@ -217,7 +217,6 @@ clear
 function inst_base {
 echo -e "\n\033[1;36mINSTALANDO O APACHE2 \033[1;33mAGUARDE...\033[0m"
 apt-get install apache2 -y > /dev/null 2>&1
-apt-get install php5 libapache2-mod-php5 php5-mysql -y > /dev/null 2>&1
 apt-get install dirmngr apt-transport-https -y > /dev/null 2>&1
 apt-get install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-zip php7.3-gd  php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json -y > /dev/null 2>&1
 apt-get install cron curl unzip -y > /dev/null 2>&1
@@ -237,9 +236,10 @@ mysql -u root -p"$pwdroot" -e "FLUSH PRIVILEGES" > /dev/null 2>&1
 echo '[mysqld]
 max_connections = 1000' >> /etc/mysql/my.cnf
 apt-get install php7.3-mysql -y > /dev/null 2>&1
-phpenmod mcrypt > /dev/null 2>&1
+php5enmod mcrypt > /dev/null 2>&1
 systemctl restart apache2 > /dev/null 2>&1
 ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin > /dev/null 2>&1
+apt-get install php7.3-ssh2 -y > /dev/null 2>&1
 apt-get install libssh2-1-dev libssh2-php -y > /dev/null 2>&1
 php -m | grep ssh2 > /dev/null 2>&1
 apt-get install php5-curl -y > /dev/null 2>&1
